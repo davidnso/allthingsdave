ca08aa12c005216eca8bac8a6914dd73efa5509a
davidnso <davidnsoesie1@gmail.com>
2019-12-11 18:46:30 -0500

    :construction: finish static home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { DesignsComponent } from './designs/designs.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    DesignsComponent,
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
